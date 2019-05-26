defaccb72c02c7d86b8bb783e79beae8664cb6ce
Honey Jill Hubahib <jillhubahib@gmail.com>
2019-05-26 20:39:25 +0800

    ActiveRecord setup

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,7 @@ APP_ROOT = File.expand_path("..", __dir__)
 
 # require the controller(s)
 Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
+# require the model(s)
+Dir.glob(File.join(APP_ROOT, 'app', 'models', '*.rb')).each { |file| require file }
+# require database configurations
+require File.join(APP_ROOT, 'config', 'database')
